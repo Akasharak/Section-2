9c9492e28540de345e17ffb2b118c8e57d30a0fe
Akasharak <132398935+Akasharak@users.noreply.github.com>
2023-06-30 23:06:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+
+    
+    <div >
+      <div>
+      <header className="App-header">
+        
+        <div>
+        <img className='insight-logo' src="images/insight.jpg" alt='logo' /></div>
+        NewsRoom
+      </header>
+      </div>
+
+      <div className='div'>
+  
+    <div id='div1' className="div1">
+      <a href="1">Home</a>
+      <a href="2">Events</a>
+      <a href="3">Sports and Culture</a>
+      <a href="4">Alumni</a>
+      <a href="5">About us</a>
+      <a href="6">Contact</a>
+      </div>
+      
+   <div id='div2' className='div2'>
+      <img className='news-image' src="images/phonathon.png" alt="news" />
+        <h1 className='h1-text'>
+        Upcoming Phonathon to be set in August.
+          </h1>     
+          <a href='7' className='a-text' >
+            Comments
+          </a>
+        
+     
+    
+    </div>
+    </div>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+
